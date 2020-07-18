91fe72ee22d8f7ea27696a1aa01a777d2f475b3b
wuliann <610575024@qq.com>
2020-07-18 17:10:50 +0800

    将api包移入这个项目

---
+++ NeteaseCloudMusicApi/module/video_group.js
@@ -0,0 +1,17 @@
+// 视频标签/分类下的视频
+
+module.exports = (query, request) => {
+  const data = {
+    groupId: query.id,
+    offset: query.offset || 0,
+    need_preview_url: 'true',
+    total: true
+  }
+  return request(
+    'POST', `https://music.163.com/api/videotimeline/videogroup/otherclient/get`, data, {
+      crypto: 'weapi',
+      cookie: query.cookie,
+      proxy: query.proxy
+    }
+  )
+}
---
+++ NeteaseCloudMusicApi/module/video_timeline_all.js
@@ -0,0 +1,19 @@
+// 全部视频列表
+
+module.exports = (query, request) => {
+  const data = {
+    groupId: 0,
+    offset: query.offset || 0,
+    need_preview_url: 'true',
+    total: true
+  }
+  console.log({data})
+  //   /api/videotimeline/otherclient/get
+  return request(
+    'POST', `https://music.163.com/api/videotimeline/otherclient/get`, data, {
+      crypto: 'weapi',
+      cookie: query.cookie,
+      proxy: query.proxy
+    }
+  )
+}
